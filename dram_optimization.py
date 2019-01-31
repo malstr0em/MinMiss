be4a6307e929e2c71e71913525a9b5c5ba10411b
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2019-01-31 17:24:51 +0100

    MinRowMissb 1st Version finished

--- dram_optimization.py
+++ dram_optimization.py
@@ -1,10 +1,6 @@
 from gurobipy import *
 from read_sequence import read_sequence
-
-m=Model()
-vars = m.addVars(10,10,vtype=GRB.BINARY)
-m.addConstrs(vars.sum(i,'*') == 2 for i in range(10))
-m.optimize()
+import numpy as np
 
 #Calculates a feasible allocation for sequence
 #param sequence : list
@@ -21,24 +17,42 @@ m.optimize()
 #the keys are the elements of the sequence and the values are the integers
 #from 1 up to number_of_rows
 def dram_optimization(sequence, number_of_banks, number_of_rows, number_of_columns):
-    print('x')
+    ts = trivial_solution(sequence14,number_of_banks,number_of_rows,number_of_columns)
+    if (number_of_banks==1):
+        print(optb(ts[0],sequence14,number_of_rows,number_of_columns))
+    else:
+        print(ts)
+
+def trivial_solution(sequence, number_of_banks, number_of_rows, number_of_columns):
+    result = np.ndarray((number_of_banks, number_of_rows, number_of_columns))
+    uniques = np.unique(sequence)
+    x=0
+    print('Setting up trivial solution')
+    for i in range(number_of_banks):
+        for j in range(number_of_rows):
+            for k in range(number_of_columns):
+                if x<len(uniques):
+                    result[i,j,k]=uniques[x]
+                    x+=1
+    print('Finished setting up trivial solution')
+    return result
 
 from minRowMissb import dram_optimization as optb
 from minRowMissc import dram_optimization as optc
 
-sequence1 = read_sequence('Sequences/10_50.seq')
-optb(sequence1,4,4)
-optc(sequence1,4,4)
-sequence2 = read_sequence('Sequences/99_500.seq')
-sequence3 = read_sequence('Sequences/190_950.seq')
-sequence4 = read_sequence('Sequences/278_1400.seq')
-sequence5 = read_sequence('Sequences/546_2750.seq')
-sequence6 = read_sequence('Sequences/792_19024.seq')
-sequence7 = read_sequence('Sequences/4224_8402.seq')
-sequence8 = read_sequence('Sequences/6121_12155.seq')
-sequence9 = read_sequence('Sequences/10336_453358.seq')
-sequence10 = read_sequence('Sequences/10400_294960.seq')
-sequence11 = read_sequence('Sequences/11296_316796.seq')
-sequence12 = read_sequence('Sequences/11520_644456.seq')
-sequence13 = read_sequence('Sequences/131072_393216.seq')
+#sequence1 = read_sequence('Sequences/10_50.seq')
+#optc(ts,sequence1,4,4)
+#sequence2 = read_sequence('Sequences/99_500.seq')
+#sequence3 = read_sequence('Sequences/190_950.seq')
+#sequence4 = read_sequence('Sequences/278_1400.seq')
+#sequence5 = read_sequence('Sequences/546_2750.seq')
+#sequence6 = read_sequence('Sequences/792_19024.seq')
+#sequence7 = read_sequence('Sequences/4224_8402.seq')
+#sequence8 = read_sequence('Sequences/6121_12155.seq')
+#sequence9 = read_sequence('Sequences/10336_453358.seq')
+#sequence10 = read_sequence('Sequences/10400_294960.seq')
+#sequence11 = read_sequence('Sequences/11296_316796.seq')
+#sequence12 = read_sequence('Sequences/11520_644456.seq')
+#sequence13 = read_sequence('Sequences/131072_393216.seq')
 sequence14 = read_sequence('Sequences/356400_1198800.seq')
+print(dram_optimization(sequence14,1,1024,512))
--- minRowMissb.py
+++ minRowMissb.py
@@ -1,33 +1,53 @@
 from gurobipy import *
 import numpy as np
 
+#solution of form bank1=[row1,row2,...,rowm]
+#row1=[col1,col2,...,colk]
 def dram_optimization(solution, sequence, number_of_rows, number_of_columns):
-    maxvariables = 10000
+    #number_of_elements = len(np.unique(sequence))
+    #maxvariables = 10000
+    maxrows = 2#max(int((maxvariables/(number_of_columns*number_of_columns+number_of_columns))**(.5)),2)
+    cursol=solution
+    if (number_of_rows<=maxrows):
+        return _dram_optimization(sequence, number_of_rows, number_of_columns)
+    else:
+        for i in range(number_of_rows-maxrows):
+            low = i
+            high = i+maxrows
+            sub = subsequence(cursol[low:high],sequence)
+            if(len(sub)!=0):
+                subsol=_dram_optimization(sub, maxrows, number_of_columns)
+                cursol=np.concatenate((cursol[:low-1],subsol,cursol[high+1:]))
+    return cursol
 
-    dram_optimization(sequence, number_of_rows, number_of_columns)
-    
-    print('x')
+def _dram_optimization(sequence, number_of_rows, number_of_columns):
+    print('*'*100)
+    print('Entered b Optimization')
+    print('Setting up Normal Form')
+    down,uniques = down_grade(sequence)
+    number_of_elements = len(uniques)
 
-def dram_optimization(sequence, number_of_rows, number_of_columns):
-    print(sequence)
-    number_of_elements = len(np.unique(sequence))
-    
+    print('Counting Misses')
     c=[]
     for i in range(number_of_elements):
         c.append([])
         for j in range(number_of_elements):
             c[i].append(0)
-    f=sequence[0]
-    for s in sequence[1:]:
+    f=down[0]
+    for s in down[1:]:
         c[f-1][s-1]+=1
         c[s-1][f-1]+=1
         f=s
-    
+
+    print('Setting up b Optimization')
     m = Model();
+    m.setParam('OutputFlag',False)
+    print('Adding Variables')
     #x[i,r]=1 object i is in row r
-    x = m.addVars(number_of_elements,number_of_rows,vtype=GRB.BINARY)
+    x = m.addVars(number_of_elements,number_of_rows,vtype=GRB.BINARY,name='x')
     #n[i,j]=c objects i and j have produced c misses
-    n = m.addVars(number_of_elements,number_of_elements)
+    n = m.addVars(number_of_elements,number_of_elements,name='n')
+    print('Adding Constraints')
     #object i can be in only one row
     m.addConstrs(x.sum(i,'*') == 1 for i in range(number_of_elements))
     #one row can hold at most number_of_columns elements
@@ -35,7 +55,26 @@ def dram_optimization(sequence, number_of_rows, number_of_columns):
     #indicator n[i,j] is set to c[i][j] if i and j are in different rows
     m.addConstrs(c[i][j]*(x[i,r] - x[j,r] - n[i,j]) <= 0  for i in range(number_of_elements) for j in range(number_of_elements) for r in range(number_of_rows))
     #count the number of misses twice because n[i][j]=n[j][i]
-    m.setObjective(1/2*n.sum('*','*'))
+    print('Adding Objective')
+    m.setObjective(1/2*n.sum('*','*'),GRB.MINIMIZE)
+    print('Optimizing')
     m.optimize()
+    x=m.getVars()
+    result = []
+    for r in range(number_of_rows):
+        result.append([])
+        for i in range(number_of_elements):
+            if x[i*number_of_rows+r].x==1:
+                result[r].append(uniques[i])
+                
+    return np.array([x+[0]*(number_of_columns-len(x)) for x in result])
+                
 
-    print('b=1 finished an execution')
+def subsequence(elements,sequence):
+    print('Getting subsequence from sequence of size '+str(len(sequence)))
+    np.ndarray.flatten(elements)
+    return [s for s in sequence for e in np.nditer(elements) if e==s]
+
+def down_grade(sequence):
+    uniques = np.unique(sequence)
+    return [i for s in sequence for i in range(len(uniques)) if s==uniques[i]],uniques
