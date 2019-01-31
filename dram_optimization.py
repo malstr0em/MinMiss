5ea86c6d73aae8bc491b1630c0dcbfc1f0713d54
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2019-01-31 14:07:26 +0100

    changed a few files for integration

--- dram_optimization.py
+++ dram_optimization.py
@@ -1,4 +1,5 @@
 from gurobipy import *
+from read_sequence import read_sequence
 
 m=Model()
 vars = m.addVars(10,10,vtype=GRB.BINARY)
@@ -21,3 +22,21 @@ m.optimize()
 #from 1 up to number_of_rows
 def dram_optimization(sequence, number_of_banks, number_of_rows, number_of_columns):
     print('x')
+
+from minRowMissb import dram_optimization as optb
+from minRowMissc import dram_optimization as optc
+
+sequence1 = read_sequence('Sequences/10_50.seq')
+sequence2 = read_sequence('Sequences/99_500.seq')
+sequence3 = read_sequence('Sequences/190_950.seq')
+sequence4 = read_sequence('Sequences/278_1400.seq')
+sequence5 = read_sequence('Sequences/546_2750.seq')
+sequence6 = read_sequence('Sequences/792_19024.seq')
+sequence7 = read_sequence('Sequences/4224_8402.seq')
+sequence8 = read_sequence('Sequences/6121_12155.seq')
+sequence9 = read_sequence('Sequences/10336_453358.seq')
+sequence10 = read_sequence('Sequences/10400_294960.seq')
+sequence11 = read_sequence('Sequences/11296_316796.seq')
+sequence12 = read_sequence('Sequences/11520_644456.seq')
+sequence13 = read_sequence('Sequences/131072_393216.seq')
+sequence14 = read_sequence('Sequences/356400_1198800.seq')
