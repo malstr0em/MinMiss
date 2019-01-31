6c0a00de239b1777fb0ec2acdcdd82d51d9a5a8f
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2019-01-31 14:12:41 +0100

    changes to the framework

--- minRowMissb.py
+++ minRowMissb.py
@@ -1,4 +1,6 @@
 from gurobipy import *
 
-def dram_optimization(sequence, number_of_rows, number_of_columns):
+def dram_optimization(solution, sequence, number_of_rows, number_of_columns):
+    maxvariables = 10000
+    
     print('x')
--- minRowMissc.py
+++ minRowMissc.py
@@ -1,4 +1,6 @@
 from gurobipy import *
 
-def dram_optimization(sequence, number_of_banks, number_of_rows):
+# receives solution of the form [bank1, bank2, ..., bankn]
+# bank1 = [row1, row2, ..., rowm]
+def dram_optimization(solution, sequence, number_of_banks, number_of_rows):
     print('x')
